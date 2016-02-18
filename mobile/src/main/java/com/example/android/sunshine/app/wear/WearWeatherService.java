d5842b3f4306fae46151b2db4ff792e11549c97f
RowlandOti <rowlandotienoo@gmail.com>
2016-02-18 16:27:37 +0300

    init: establish new repo for Ubiquitous app project

---
+++ mobile/src/main/java/com/example/android/sunshine/app/wear/WearWeatherService.java
@@ -0,0 +1,29 @@
+package com.example.android.sunshine.app.wear;
+
+import android.util.Log;
+
+import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class WearWeatherService extends WearableListenerService {
+
+    // Logging Identifier for the class
+    private static final String LOG_TAG = WearWeatherService.class.getSimpleName();
+
+    private static final String WEATHER_PATH = "/weather";
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        for (DataEvent dataEvent : dataEvents) {
+            if (dataEvent.getType() == DataEvent.TYPE_CHANGED) {
+                String path = dataEvent.getDataItem().getUri().getPath();
+                Log.d(LOG_TAG, path);
+                if (path.equals(WEATHER_PATH)) {
+                    SunshineSyncAdapter.syncImmediately(this);
+                }
+            }
+        }
+    }
+}
